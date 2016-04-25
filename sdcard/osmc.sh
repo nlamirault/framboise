72cdafe02b0000cf572a3b54db31a36ed21dfb8b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-04-25 09:00:49 +0200

    Add OSMC script
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ sdcard/osmc.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Copyright (C) 2016 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+NO_COLOR="\033[0m"
+OK_COLOR="\033[32;01m"
+ERROR_COLOR="\033[31;01m"
+WARN_COLOR="\033[33;01m"
+
+osmc_version="2016.04-1"
+osmc_img_version="20160403"
+
+echo -e "${OK_COLOR}== OSMC ${osmc_version} for Raspberry Pi 2 ==${NO_COLOR}"
+if [ $# -eq 0 ]; then
+    echo -e "${ERROR_COLOR}Usage: $0 sdX${NO_COLOR}"
+    exit 1
+fi
+
+sdcard="/dev/$1"
+echo -e "${WARN_COLOR}Use sdcard :${NO_COLOR} ${sdcard}"
+
+echo -e "${WARN_COLOR}Downloading the OSMC root filesystem${NO_COLOR}"
+if [ ! -f "OSMC_TGT_rbp2_${osmc_img_version}.img.gz" ]; then
+    curl -LO --progress-bar http://download.osmc.tv/installers/diskimages/OSMC_TGT_rbp2_${osmc_img_version}.img.gz
+fi
+
+echo -e "${WARN_COLOR}Setup SD Card${NO_COLOR}"
+parted -s ${sdcard} unit s print
+parted ${sdcard} mkpart primary fat32 0 100%
+
+echo -e "${WARN_COLOR}Unmounting${NO_COLOR}"
+umount ${sdcard}1
+
+echo -e "${WARN_COLOR}Extracting the OSMC image${NO_COLOR}"
+if [ ! -f "OSMC_TGT_rpb2_${osmc_img_version}.img" ]; then
+     gunzip -d OSMC_TGT_rpb2_${osmc_img_version}.img.gz
+fi
+
+echo -e "${WARN_COLOR}Installing OSMC to SD Card${NO_COLOR}"
+dd if=OSMC_TGT_rpb2_${osmc_img_version}.img of=${sdcard} bs=4M
+sync
+
+echo -e "${OK_COLOR}== Done ==${NO_COLOR}"
